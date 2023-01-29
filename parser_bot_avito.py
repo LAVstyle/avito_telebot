0965a02180dfcbc7488dce222b7222ad0058b364
LAVstyle <luchin250398@yandex.ru>
2023-01-29 16:15:43 +0700

    Took Firefox

--- parser_bot_avito.py
+++ parser_bot_avito.py
@@ -1,22 +1,21 @@
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 
 
-url = "https://www.avito.ru/tomsk/kvartiry/sdam/na_dlitelnyy_srok-ASgBAgICAkSSA8gQ8AeQUg?cd=1&s=104"
+def get_source_html(url):
 
-
-def get_sourse_html():
-
-    url = "https://www.avito.ru/tomsk/kvartiry/sdam/na_dlitelnyy_srok-ASgBAgICAkSSA8gQ8AeQUg?cd=1&s=104"
-    driver  = webdriver.Edge(executable_path="D:\!!_AVL\Desktop\COMPUTER_SCIENCE\Python\Bots\Bot_3\msedgedriver.exe")
-    driver.maximize_window()
+    # url = "https://www.avito.ru/tomsk/kvartiry/sdam/na_dlitelnyy_srok-ASgBAgICAkSSA8gQ8AeQUg?cd=1&s=104"
+    driver  = webdriver.Firefox(executable_path="D:\!!_AVL\Desktop\COMPUTER_SCIENCE\Python\Bots\Bot_3\geckodriver.exe")
+    # driver.maximize_window()
 
     try:
         driver.get(url=url)
-        time.sleep(4)
+        time.sleep(7)
 
-        with open("")   ###################################
+        with open("index_selenium.html", 'w', encoding="utf-8") as file:
+            file.write(driver.page_source)
+
     except Exception as _ex:
         print(_ex)
     finally:
@@ -24,19 +23,20 @@ def get_sourse_html():
         driver.quit()
 
 
-def get_items_urls(file_path):
-    with open(file_path) as file:
-        src = file.read()
+## def get_items_urls(file_path):
+#     with open(file_path) as file:
+#         src = file.read()
 
-        bs = BeautifulSoup(src, "lxml")
+#         bs = BeautifulSoup(src, "lxml")
 
-        all_links = bs.find_all("a", class_="iva-item-title")
+#         all_links = bs.find_all("a", class_="iva-item-title")
 
-        for link in all_links:
-            return link
+#         for link in all_links:
+#             return link
 
 def main():
-    print(get_items_urls(file_path=""))
+    get_source_html("https://www.avito.ru/tomsk/kvartiry/sdam/na_dlitelnyy_srok-ASgBAgICAkSSA8gQ8AeQUg?cd=1&s=104")
+    # print(get_items_urls(file_path=""))
 
 
 if __name__=="__main__":
